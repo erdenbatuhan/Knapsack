c86932960e40eba2dc3dadc57526a6086b717c2e
erdenbatuhan <erden.batuhan@gmail.com>
2017-04-12 01:51:46 +0300

    Initial Commit

---
+++ 0-1 Knapsack/0-1 Knapsack/main.cpp
@@ -0,0 +1,99 @@
+//
+//  main.cpp
+//  0-1 Knapsack
+//
+//  Created by Batuhan Erden on 02/04/2017.
+//  Copyright © 2017 Batuhan Erden. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#define MAX_N   101
+
+using namespace std;
+
+int PureRecursiveKnapsack(int n, int capacity, int weights[], int values[]) {
+    if (n == 0 || capacity == 0)
+        return 0;
+    
+    if (capacity < weights[n])
+        return PureRecursiveKnapsack(n - 1, capacity, weights, values);
+    else
+        return max(PureRecursiveKnapsack(n - 1, capacity, weights, values), values[n] + PureRecursiveKnapsack(n - 1, capacity - weights[n], weights, values));
+}
+
+int MemoryFunctionKnapsack(int M[MAX_N][MAX_N], int n, int capacity, int weights[], int values[]) {
+    if (M[n][capacity] < 0) {
+        if (capacity < weights[n]) {
+            M[n][capacity] = MemoryFunctionKnapsack(M, n - 1, capacity, weights, values);
+        } else {
+            M[n][capacity] = max(MemoryFunctionKnapsack(M, n - 1, capacity, weights, values), values[n] + MemoryFunctionKnapsack(M, n - 1, capacity - weights[n], weights, values));
+        }
+    }
+    
+    return M[n][capacity];
+}
+
+int MemoryFunctionKnapsack(int n, int capacity, int weights[], int values[]) {
+    int M[MAX_N][MAX_N];
+    
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= capacity; j++)
+            M[i][j] = -1;
+    
+    M[0][0] = 0;
+    
+    return MemoryFunctionKnapsack(M, n, capacity, weights, values);
+}
+
+int Knapsack(int n, int capacity, int weights[], int values[]) {
+    int M[n + 1][capacity + 1];
+    
+    for (int i = 0; i <= n; i++)
+        M[i][0] = 0;
+    
+    for (int w = 0; w <= capacity; w++)
+        M[0][w] = 0;
+    
+    for (int i = 1; i <= n; i++)
+        for (int w = 0; w <= capacity; w++)
+            if (weights[i] <= w)
+                M[i][w] = max(M[i - 1][w], values[i] + M[i - 1][w - weights[i]]);
+            else
+                M[i][w] = M[i - 1][w];
+    
+    return M[n][capacity];
+}
+
+void readInput(int &n, int &capacity, int weights[], int values[]) {
+    freopen("/Users/batuhan/Documents/Xcode Workspace/0-1 Knapsack/items", "r", stdin);
+    
+    scanf("%d", &capacity);
+    while (scanf("%d%d", &weights[n], &values[n]) == 2)
+        n++;
+    n--;
+}
+
+int main(int argc, const char * argv[]) {
+    int n = 1, capacity = 0;
+    int weights[MAX_N], values[MAX_N];
+    
+    readInput(n, capacity, weights, values);
+    
+    printf("Optimal Weight (bottom-up): %d\n", Knapsack(n, capacity, weights, values));
+    printf("Optimal Weight (top-down, Memoization): %d\n", MemoryFunctionKnapsack(n, capacity, weights, values));
+    printf("Optimal Weight (top-down, Pure Recursion): %d\n", PureRecursiveKnapsack(n, capacity, weights, values));
+    return 0;
+}
+
+/*
+ Sample items file
+ -----------------
+ 11
+ 1 1
+ 2 6
+ 5 18
+ 6 22
+ 7 28
+ -----------------
+ */
---
+++ Fractional Knapsack/Fractional Knapsack/main.cpp
@@ -0,0 +1,81 @@
+//
+//  main.cpp
+//  Fractional Knapsack
+//
+//  Created by Batuhan Erden on 31/03/2017.
+//  Copyright © 2017 Batuhan Erden. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#define MAX_N   101
+#define INF     std::numeric_limits<int>::infinity()
+
+struct Spice {
+    double weight, value, density;
+};
+
+struct DensityComparator {
+    bool operator() (const Spice &a, const Spice &b) {
+        return a.density > b.density;
+    }
+};
+
+double FractionalKnapsack(int n, int capacity, double weights[], double values[]) {
+    Spice spices[MAX_N];
+    double M[MAX_N], collected = 0;
+    
+    for (int i = 0; i <= n; i++) {
+        spices[i].weight = weights[i];
+        spices[i].value = values[i];
+        spices[i].density = values[i] / weights[i];
+    }
+    
+    std::sort(spices, spices + n + 1, DensityComparator());
+    
+    for (int i = 1; i <= n; i++) {
+        if (collected + spices[i].weight > capacity) {
+            double partial = (capacity - collected) * spices[i].density;
+            M[i] = partial + M[i - 1];
+            
+            return M[i];
+        }
+        
+        M[i] = spices[i].value + M[i - 1];
+        collected += spices[i].weight;
+    }
+    
+    return M[n];
+}
+
+void readInput(int &n, int &capacity, double weights[], double values[]) {
+    freopen("/Users/batuhan/Documents/Xcode Workspace/Fractional Knapsack/items", "r", stdin);
+    
+    scanf("%d", &capacity);
+    while (scanf("%lf%lf", &weights[n], &values[n]) == 2)
+        n++;
+    n--;
+}
+
+int main(int argc, const char * argv[]) {
+    int n = 1, capacity = 0;
+    double weights[MAX_N], values[MAX_N];
+    weights[0] = INF, values[0] = INF;
+    
+    readInput(n, capacity, weights, values);
+    
+    printf("Optimal Weight: %f\n", FractionalKnapsack(n, capacity, weights, values));
+    return 0;
+}
+
+/*
+ Sample items file
+ -----------------
+ 17
+ 3 4
+ 4 5
+ 7 10
+ 8 11
+ 9 13
+ -----------------
+ */
